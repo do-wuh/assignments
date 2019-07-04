68bc020e69c1b08f66b7bdea235378216043ca55
eskil tomozy <eskil@tomozy.com>
2019-07-04 03:24:24 +0000

    Assignments:Blur#1 - initial commit

---
+++ blur-1-tomozy.rb
@@ -0,0 +1,20 @@
+class Image
+
+  def initialize(pixel)
+    @pixel = pixel
+  end 
+
+  def output_image
+    @pixel.each do |row|
+        puts row.join(" ")
+    end
+  end
+
+end
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+image.output_image
